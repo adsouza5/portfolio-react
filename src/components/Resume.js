f960b97cc65cd0debe29c959141745b6422f3600
Adam Dsouza <99364846+adsouza5@users.noreply.github.com>
2025-03-23 14:36:59 +0000

    new changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,7 +7,7 @@ const Resume = () => {
       <h2 className="h1-text">Resume</h2>
       <div className="flex justify-center">
         <img
-          src= {"/resume.jpg"}
+          src= {"/images/resume.jpg"}
           alt="Adam Dsouza Resume"
           className="shadow-lg w-3/4 mx-auto card-hover"
           id="zoomableImage"
